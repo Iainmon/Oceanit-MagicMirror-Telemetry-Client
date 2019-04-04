cdba97eb1c73be29bdb43113c440cc59c7221829
Iain Moncrief <Iainmoncrief@gmail.com>
2019-04-03 15:44:35 -1000

    added times to deposit package

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 var request = require('request');
-const uuidv3 = require('uuid/v3');
+const uuidv1 = require('uuid/v1');
 var Thread = require('async-threading');
+const fs = require('fs');
 
 const config = require('./telemetry.json');
 
-const hostString = `https://${config.host}` ;
+const hostString = `https://${config.host}`;
+
+const telemStartTimeMilis = Date.now();
+const telemStartTime = new Date();
+
+var uuid = null;
+try {
+    uuid = fs.readFileSync('./uuid.txt');
+} catch (e) {
+    uuid = null;
+}
+if (!uuid) {
+    uuid = uuidv1();
+    try {
+        fs.writeFileSync('./uuid.txt', uuid);
+    } catch (e) {
+        uuid = 'ERROR! - Device failed to save uuid!';
+    }
+}
+
+
 
 var telemetryThread = new Thread(
     () => {
@@ -12,14 +33,16 @@ var telemetryThread = new Thread(
             {
                 url : hostString + '/deposit',
                 form : {
-                    uuid : uuidv3('arbitrary-register.grape-juice.org', uuidv3.DNS),
-                    version : '1.0'
+                    uuid : uuid,
+                    version : '1.0',
+                    uptime : ((Date.now()) - telemStartTimeMilis),
+                    upSince : telemStartTime
                 }
             },
             function (error, response, body) {
                 console.log(body);
                 if (!error && response.statusCode == 200) {
-                    console.log('')
+                    console.log('');
                 } else {
                     console.log('Error sending telemetry! Please contact <iainmoncrief@gmail.com>');
                 }
@@ -27,6 +50,7 @@ var telemetryThread = new Thread(
         );
     },
     1000 * 60 * 60 * 4,
+    //100,
     true
 );
 
